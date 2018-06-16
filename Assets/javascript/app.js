ce273d0a591a661e20cd6ec0356c88a192461a41
Tyler Bisbee <Tyler.Bisbee@gmail.com>
2018-06-16 10:12:40 -0400

    files with materialize

---
+++ Assets/javascript/app.js
@@ -0,0 +1,22 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA9g_qC6yTTKNtOkXUYbl87aV66XxgKpAI",
+    authDomain: "roshambo-tb.firebaseapp.com",
+    databaseURL: "https://roshambo-tb.firebaseio.com",
+    projectId: "roshambo-tb",
+    storageBucket: "",
+    messagingSenderId: "995835380271"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+      
+
